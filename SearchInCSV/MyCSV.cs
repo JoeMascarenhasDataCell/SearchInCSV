71c5ba278f29473c3e2896b69eb93d8d3c6f8cc6
JoeMascarenhasDataCell <joe.m@datacellme.com>
2022-09-13 12:56:14 +0400

    Add project files.

---
+++ SearchInCSV/MyCSV.cs
@@ -0,0 +1,58 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+
+namespace SearchInCSV
+{
+    internal class MyCSV
+    {
+        public string? FilePath { get; set; }
+
+        public MyCSV(string filePath)
+        {
+            this.FilePath = filePath;
+        }
+
+        public MyCSV()
+        {
+        }
+
+        public void FindInFileAndShow(int columnIndex, string searchKey)
+        {
+            try
+            {
+                if (!String.IsNullOrEmpty(FilePath))
+                {
+                    bool notFound = true;
+
+                    CsvConfiguration csvConfig = new(CultureInfo.CurrentCulture)
+                    {
+                        HasHeaderRecord = false
+                    };
+
+                    using StreamReader? streamReader = File.OpenText(FilePath);
+                    using CsvReader? csvReader = new CsvReader(streamReader, csvConfig);
+                    while (csvReader.Read())
+                    {
+                        bool gotField = csvReader.TryGetField<string>(columnIndex, out string searchColumnValue);
+
+                        if (gotField && searchColumnValue == searchKey)
+                        {
+                            notFound = false;
+                            for (int i = 0; csvReader.TryGetField<string>(i, out string value); i++)
+                                Console.Write($"{value} ");
+                            Console.WriteLine();
+                        }
+                    }
+
+                    if (notFound)
+                        Console.WriteLine("No data found");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something Went Wrong 1");
+            }
+        }
+    }
+}
---
+++ SearchInCSV/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace SearchInCSV
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            try
+            {
+                if (args.Length != 3)
+                {
+                    Console.WriteLine("Invalid number of arguements");
+                    return 1;
+                }
+
+                if (!Int32.TryParse(args[1], out int columnIndex))
+                {
+                    Console.WriteLine("Column index must be a positive integer");
+                    return 1;
+                }
+
+                string fileName = args[0];
+                string searchKey = args[2];
+
+                if (fileName.Split('.').Last() != "csv")
+                {
+                    Console.WriteLine("Invalid file type");
+                    return 1;
+                }
+
+                if (!File.Exists(fileName))
+                {
+                    Console.WriteLine("File not found");
+                    return 1;
+                }
+
+                if (columnIndex < 0)
+                {
+                    Console.WriteLine("Column index must be a positive integer");
+                    return 1;
+                }
+
+                MyCSV myCSV = new(fileName);
+                myCSV.FindInFileAndShow(columnIndex, searchKey);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something Went Wrong");
+            }
+            return 0;
+        }
+    }
+}
